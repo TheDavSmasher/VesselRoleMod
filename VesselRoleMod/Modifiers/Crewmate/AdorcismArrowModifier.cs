be5821df135e6c8f42f659a43354820f0750deb1
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-04-26 01:43:22 -0600

    Added Adoricism Arrow Modifier

---
+++ VesselRoleMod/Modifiers/Crewmate/AdorcismArrowModifier.cs
@@ -0,0 +1,34 @@
+﻿using MiraAPI.GameOptions;
+using TownOfUs.Modifiers;
+using TownOfUs.Modules.RainbowMod;
+using UnityEngine;
+using VesselRoleMod.Options.Roles.Crewmate;
+
+namespace VesselRoleMod.Modifiers.Crewmate;
+
+/// <summary>
+/// Modifier added to the Vessel when performing an Adorcism, which gives an arrow to all ghosts that can possess.
+/// </summary>
+/// <param name="owner">The ghost player that can see the arrow</param>
+public sealed class AdorcismArrowModifier(PlayerControl owner, Color color)
+	: ArrowTargetModifier(owner, color, 0)
+{
+	public override float Duration => OptionGroupSingleton<VesselOptions>.Instance.AdorciseWindow;
+
+	public override bool AutoStart => true;
+
+	public override void OnActivate()
+	{
+		base.OnActivate();
+
+		if (Arrow == null)
+		{
+			return;
+		}
+
+		var spr = Arrow.gameObject.GetComponent<SpriteRenderer>();
+		var r = Arrow.gameObject.AddComponent<BasicRainbowBehaviour>();
+
+		r.AddRend(spr, Player.cosmetics.ColorId);
+	}
+}
