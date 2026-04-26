fe9571c0551a18190d773a417cfb634dac1e6ca2
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-04-26 01:43:46 -0600

    Added empty Adorcism modifier

---
+++ VesselRoleMod/Modifiers/Crewmate/VesselAdorcismModifier.cs
@@ -0,0 +1,12 @@
+﻿using MiraAPI.GameOptions;
+using MiraAPI.Modifiers.Types;
+using VesselRoleMod.Options.Roles.Crewmate;
+
+namespace VesselRoleMod.Modifiers.Crewmate;
+
+public sealed class VesselAdorcismModifier : TimedModifier
+{
+	public override float Duration => OptionGroupSingleton<VesselOptions>.Instance.AdorciseWindow;
+
+	public override string ModifierName => "Vessel Adorcism";
+}
