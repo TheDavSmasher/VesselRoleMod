ddfe206bae764025ef871811dcecc7dea50e051f
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-04-27 13:12:45 -0600

    Used TOU assets as step-ins

--- VesselRoleMod/Assets/VesselCrewAssets.cs
+++ VesselRoleMod/Assets/VesselCrewAssets.cs
@@ -1,11 +1,12 @@
 ﻿using MiraAPI.Utilities.Assets;
+using TownOfUs.Assets;
 using UnityEngine;
 
 namespace VesselRoleMod.Assets;
 
 public static class VesselCrewAssets
 {
-	public static LoadableAsset<Sprite> AdorciseSprite { get; } = null;
+	public static LoadableAsset<Sprite> AdorciseSprite { get; } = TouCrewAssets.MediateSprite;
 
-	public static LoadableAsset<Sprite> ExorciseSprite { get; } = null;
+	public static LoadableAsset<Sprite> ExorciseSprite { get; } = TouCrewAssets.MediateSprite;
 }
--- VesselRoleMod/Assets/VesselModAssets.cs
+++ VesselRoleMod/Assets/VesselModAssets.cs
@@ -1,11 +1,12 @@
 ﻿using MiraAPI.Utilities.Assets;
+using TownOfUs.Assets;
 using UnityEngine;
 
 namespace VesselRoleMod.Assets;
 
 public static class VesselModAssets
 {
-	public static LoadableAsset<Sprite> VesselBlockedSprite { get; } = null;
+	public static LoadableAsset<Sprite> VesselBlockedSprite { get; } = TouAssets.ImitateDeselectSprite;
 
-	public static LoadableAsset<Sprite> VesselUnblockedSprite { get; } = null;
+	public static LoadableAsset<Sprite> VesselUnblockedSprite { get; } = TouAssets.ImitateSelectSprite;
 }
