ba279d784722615e8486db4606c4abf87bc19df9
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-04-28 21:25:10 -0600

    Added kill modifier and kill button for killing ghosts

---
+++ VesselRoleMod/Buttons/Modifiers/VesselPossessionKillButton.cs
@@ -0,0 +1,60 @@
+﻿using MiraAPI.Keybinds;
+using MiraAPI.Modifiers;
+using MiraAPI.Utilities.Assets;
+using Reactor.Utilities.Extensions;
+using TownOfUs.Assets;
+using TownOfUs.Buttons;
+using TownOfUs.Networking;
+using TownOfUs.Utilities;
+using UnityEngine;
+using VesselRoleMod.Modifiers.Crewmate;
+
+namespace VesselRoleMod.Buttons.Modifiers;
+
+public sealed class VesselPossessionKillButton : TownOfUsTargetButton<PlayerControl>, IDiseaseableButton, IKillButton
+{
+	public override string Name => "Kill";
+	public override BaseKeybind Keybind => Keybinds.PrimaryAction;
+	public override float Cooldown => PlayerControl.LocalPlayer.GetKillCooldown();
+	public override LoadableAsset<Sprite> Sprite => TouAssets.KillSprite;
+
+	private static PlayerControl? Vessel => PlayerControl.LocalPlayer.GetModifier<GhostPossessorModifier>()?.Vessel;
+
+	public override bool Enabled(RoleBehaviour? role)
+	{
+		return PlayerControl.LocalPlayer != null &&
+			   PlayerControl.LocalPlayer.Data.IsDead &&
+			   PlayerControl.LocalPlayer.HasModifier<GhostPossessorModifier>();
+	}
+
+	public override PlayerControl? GetTarget()
+	{
+		return Vessel?.GetClosestLivingPlayer(true, Distance);
+	}
+
+	public void SetDiseasedTimer(float multiplier)
+	{
+		SetTimer(Cooldown * multiplier);
+	}
+
+	public override void SetOutline(bool active)
+	{
+		if (Target != null && PlayerControl.LocalPlayer.HasDied())
+		{
+			Target.cosmetics.currentBodySprite.BodySprite.SetOutline(active ? VesselRoleModColors.Vessel : null);
+		}
+	}
+
+	protected override void OnClick()
+	{
+		if (Target == null)
+		{
+			return;
+		}
+
+		PlayerControl.LocalPlayer.RpcFramedMurder(
+			Target,
+			Vessel!,
+			causeOfDeath: "VesselPossession");
+	}
+}
---
+++ VesselRoleMod/Modifiers/Crewmate/GhostPossessorModifier.cs
@@ -0,0 +1,18 @@
+﻿using MiraAPI.Modifiers;
+using VesselRoleMod.Roles.Crewmate;
+
+namespace VesselRoleMod.Modifiers.Crewmate;
+
+public sealed class GhostPossessorModifier(PlayerControl vessel) : BaseModifier
+{
+	public override string ModifierName => "Ghost Possessor";
+
+	public override bool HideOnUi => true;
+
+	public PlayerControl Vessel => vessel;
+
+	public bool IsKillingGhost()
+	{
+		return vessel.Data.Role is VesselRole;
+	}
+}
