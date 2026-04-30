bbea946a2f0eb32c125e23f0ab00261da813416d
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-04-30 16:02:47 -0600

    Added stubbed Role icon asset sprite

---
+++ VesselRoleMod/Assets/VesselRoleIcons.cs
@@ -0,0 +1,10 @@
+﻿using MiraAPI.Utilities.Assets;
+using TownOfUs.Assets;
+using UnityEngine;
+
+namespace VesselRoleMod.Assets;
+
+public static class VesselRoleIcons
+{
+	public static LoadableAsset<Sprite> Vessel { get; } = TouRoleIcons.Medium;
+}
