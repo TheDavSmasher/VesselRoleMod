a91b60fff55127c2259b4d8bd7660316397e8a3c
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-05-04 16:13:06 -0600

    Changed patch to only additionally handle Poltergeists

--- VesselRoleMod/Patches/ButtonClickPatches.cs
+++ VesselRoleMod/Patches/ButtonClickPatches.cs
@@ -1,47 +1,21 @@
 ﻿using HarmonyLib;
 using MiraAPI.Modifiers;
-using System.Linq;
-using TownOfUs.Modifiers;
 using VesselRoleMod.Modifiers.Ghost;
-using static TownOfUs.Patches.ButtonClickPatches;
 
 namespace VesselRoleMod.Patches;
 
 [HarmonyPatch]
 public static class ButtonClickPatches
 {
-	[HarmonyPatch(typeof(ReportButton), nameof(ReportButton.DoClick))]
 	[HarmonyPatch(typeof(UseButton), nameof(UseButton.DoClick))]
-	[HarmonyPatch(typeof(PetButton), nameof(PetButton.DoClick))]
-	[HarmonyPriority(Priority.First)]
+	[HarmonyPriority(799)] // second
 	[HarmonyPrefix]
-	public static bool VanillaButtonChecks(ActionButton __instance)
+	public static bool VanillaButtonChecks(UseButton __instance)
 	{
-		if (!CanUseAbilities())
+		if (PlayerControl.LocalPlayer != null &&
+			PlayerControl.LocalPlayer.TryGetModifier<PoltergeistModifier>(out var mod) && mod.Vessel != null)
 		{
-			return false;
-		}
-
-		if (PlayerControl.LocalPlayer != null)
-		{
-			var disabledMods = PlayerControl.LocalPlayer.GetModifiers<DisabledModifier>();
-			if (__instance is UseButton)
-			{
-				if (PlayerControl.LocalPlayer.TryGetModifier<PoltergeistModifier>(out var mod) && mod.Vessel != null)
-				{
-					return true;
-				}
-			}
-
-			if (__instance is ReportButton && disabledMods.Any(x => !x.CanReport))
-			{
-				return false;
-			}
-
-			if (disabledMods.Any(x => !x.CanUseConsoles))
-			{
-				return false;
-			}
+			return true;
 		}
 
 		return true;
