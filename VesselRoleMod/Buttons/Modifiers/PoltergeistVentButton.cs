1361a314cb18c0f93bbf96637f77fc28df31bea0
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-05-22 18:49:06 -0600

    Updated vent button with more logic from engineer and miner

--- VesselRoleMod/Buttons/Modifiers/PoltergeistVentButton.cs
+++ VesselRoleMod/Buttons/Modifiers/PoltergeistVentButton.cs
@@ -1,8 +1,9 @@
-﻿using MiraAPI.Hud;
+﻿using MiraAPI.Events;
+using MiraAPI.Events.Vanilla.Usables;
+using MiraAPI.Hud;
 using MiraAPI.Keybinds;
 using MiraAPI.Modifiers;
 using MiraAPI.Utilities.Assets;
-using Rewired;
 using System.Linq;
 using TownOfUs.Assets;
 using TownOfUs.Buttons;
@@ -10,9 +11,12 @@ using TownOfUs.Buttons.Crewmate;
 using TownOfUs.Modifiers;
 using TownOfUs.Modifiers.Neutral;
 using TownOfUs.Modules;
+using TownOfUs.Roles.Impostor;
+using TownOfUs.Roles.Neutral;
 using TownOfUs.Utilities;
 using UnityEngine;
 using VesselRoleMod.Modifiers.Ghost;
+using VesselRoleMod.Utilities;
 
 namespace VesselRoleMod.Buttons.Modifiers;
 
@@ -42,7 +46,7 @@ public sealed class PoltergeistVentButton : TownOfUsTargetButton<Vent>
 
 	public override Vent? GetTarget()
 	{
-		return TouRoleUtils.GetClosestUsableVent(true);
+		return Vessel?.GetClosestUsableVent(true);
 	}
 
 	public override void SetOutline(bool active)
@@ -75,8 +79,12 @@ public sealed class PoltergeistVentButton : TownOfUsTargetButton<Vent>
 			return false;
 		}
 
-		return (Timer <= 0 && !PlayerControl.LocalPlayer.inVent && Target != null) ||
-				PlayerControl.LocalPlayer.inVent;
+		if (Vessel == null)
+		{
+			return false;
+		}
+
+		return (Timer <= 0 && !Vessel.inVent && Target != null) || Vessel.inVent;
 	}
 
 	public override void ClickHandler()
@@ -100,17 +108,94 @@ public sealed class PoltergeistVentButton : TownOfUsTargetButton<Vent>
 		}
 		else
 		{
-			Timer = !PlayerControl.LocalPlayer.inVent ? 0.001f : Cooldown;
+			Timer = !Vessel!.inVent ? 0.001f : Cooldown;
 		}
 	}
 
 	protected override void OnClick()
 	{
-		throw new System.NotImplementedException();
+		if (Vessel != null && Vessel.inVent)
+		{
+			if (Target != null)
+			{
+				Vessel.MyPhysics.RpcEnterVent(Target.Id);
+				if (Role is not JesterRole)
+				{
+					Target.SetButtons(true);
+				}
+				if (Role is MinerRole)
+				{
+					HandleMinerVents();
+				}
+			}
+		}
+		else if (Timer != 0)
+		{
+			OnEffectEnd();
+			if (!HasEffect)
+			{
+				EffectActive = false;
+				Timer = Cooldown;
+			}
+		}
+	}
+
+	private void HandleMinerVents()
+	{
+		if (Target == null || !Target.name.Contains("MinerVent"))
+		{
+			return;
+		}
+
+		Vent[] nearbyVents = Target.NearbyVents;
+		for (var i = 0; i < Target.Buttons.Length; i++)
+		{
+			var buttonBehavior = Target.Buttons[i];
+			var vent = nearbyVents[i];
+
+			if (vent != null && !vent.myRend.enabled)
+			{
+				buttonBehavior.gameObject.SetActive(false);
+			}
+		}
 	}
 
 	public override void OnEffectEnd()
 	{
-		base.OnEffectEnd();
+		if (Vessel == null || !Vessel.inVent)
+		{
+			return;
+		}
+
+		_ = Vent.currentVent.CanUse(Vessel.Data, true, out var couldUse);
+		Vent.currentVent.SetButtons(false);
+
+		if (!couldUse)
+		{
+			Error($"Current vent cannot be exited, finding alternate route.");
+			Vent? newVent = null;
+			foreach (var closeVent in Vent.currentVent.NearbyVents)
+			{
+				if (newVent != null)
+				{
+					break;
+				}
+				var @event = new PlayerCanUseEvent(closeVent.Cast<IUsable>());
+				MiraEventManager.InvokeEvent(@event);
+
+				if (!@event.IsCancelled)
+				{
+					newVent = closeVent;
+				}
+			}
+
+			if (newVent != null)
+			{
+				Vessel.MyPhysics.RpcExitVent(newVent.Id);
+				return;
+			}
+		}
+
+		Vessel.MyPhysics.RpcExitVent(Vent.currentVent.Id);
 	}
 }
