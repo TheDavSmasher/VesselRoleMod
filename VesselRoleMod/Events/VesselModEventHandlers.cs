3a5b670f2a614ffe716933854844808307a555de
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-05-26 23:25:50 -0600

    Added PlayerLeaveEvent for BlockedMeetingMenu

---
+++ VesselRoleMod/Events/VesselModEventHandlers.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using MiraAPI.Events;
+using MiraAPI.Events.Vanilla.Player;
+using System.Linq;
+using VesselRoleMod.Modules;
+
+namespace VesselRoleMod.Events;
+
+public static class VesselModEventHandlers
+{
+	[RegisterEvent]
+	public static void PlayerLeaveEventHandler(PlayerLeaveEvent @event)
+	{
+		if (!MeetingHud.Instance)
+		{
+			return;
+		}
+
+		var player = @event.ClientData.Character;
+
+		if (!player)
+		{
+			return;
+		}
+
+		var pva = MeetingHud.Instance.playerStates.First(x => x.TargetPlayerId == player.PlayerId);
+
+		if (!pva)
+		{
+			return;
+		}
+
+		BlockedMeetingMenu.Instances.Do(x => x.HideSingle(player.PlayerId));
+	}
+}
