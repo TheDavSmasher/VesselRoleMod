c22d4d653668c097dee41f10161aff19246634f2
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-05-28 11:41:25 -0600

    Removed HasControl case that is controlled by CanVent

--- VesselRoleMod/Buttons/Modifiers/PoltergeistVentButton.cs
+++ VesselRoleMod/Buttons/Modifiers/PoltergeistVentButton.cs
@@ -46,11 +46,6 @@ public sealed class PoltergeistVentButton : PoltergeistTargetButton<PoltergeistM
 			return Vent.currentVent;
 		}
 
-		if (!VesselControlState.HasControl(PlayerControl.LocalPlayer.PlayerId))
-		{
-			return null;
-		}
-
 		return DestroyableSingleton<HudManager>.Instance.ImpostorVentButton.currentTarget;
 	}
 
