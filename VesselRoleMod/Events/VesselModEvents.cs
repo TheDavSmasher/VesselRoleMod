a676b1e067413bb08db2203ea6ce263d74622863
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-06-02 19:11:41 -0600

    Added event to fix buttons only updating on Lobby start

---
+++ VesselRoleMod/Events/VesselModEvents.cs
@@ -0,0 +1,35 @@
+﻿using MiraAPI.Events;
+using MiraAPI.Events.Vanilla.Gameplay;
+using MiraAPI.Hud;
+
+namespace VesselRoleMod.Events;
+
+public static class VesselModEvents
+{
+	[RegisterEvent]
+	public static void UpdateButtonUses(RoundStartEvent @event)
+	{
+		if (!@event.TriggeredByIntro)
+		{
+			return;
+		}
+
+		foreach (var button in CustomButtonManager.Buttons)
+		{
+			if (button?.Button == null)
+			{
+				continue;
+			}
+
+			button.Button.name = button.Name + "Button";
+			button.Button.OverrideText(button.Name.ToUpperInvariant());
+
+			button.Button.graphic.sprite = button.Sprite.LoadAsset();
+			button.Button.SetUsesRemaining(button.MaxUses);
+			if (button.MaxUses <= 0)
+			{
+				button.Button.SetInfiniteUses();
+			}
+		}
+	}
+}
