dd8579fed21e38ff42ea529d775706c66fa3527a
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-06-03 21:05:57 -0600

    Added patches for events and rpcs that would otherwise pass the ghost instead of the vessel

--- VesselRoleMod/Patches/ControlSystem/RoleSpecificPatches.cs
+++ VesselRoleMod/Patches/ControlSystem/RoleSpecificPatches.cs
@@ -1,8 +1,21 @@
 ﻿using HarmonyLib;
+using MiraAPI.Events.Vanilla.Gameplay;
+using MiraAPI.GameOptions;
+using MiraAPI.Modifiers;
+using MiraAPI.Networking;
+using TownOfUs.Events.Crewmate;
+using TownOfUs.Events.Modifiers;
+using TownOfUs.Modifiers.Game.Crewmate;
+using TownOfUs.Modules;
 using TownOfUs.Modules.ControlSystem;
+using TownOfUs.Options.Roles.Crewmate;
+using TownOfUs.Roles;
+using TownOfUs.Roles.Crewmate;
 using TownOfUs.Roles.Impostor;
+using TownOfUs.Roles.Neutral;
 using TownOfUs.Utilities;
 using VesselRoleMod.Modifiers.Crewmate;
+using VesselRoleMod.Modifiers.Ghost;
 using VesselRoleMod.Utilities;
 
 
@@ -25,6 +38,172 @@ public static class RoleSpecificPatches
 			PuppeteerControlState.IsControlled(target.PlayerId, out _))
 		{
 			target.RemoveExistingModifier<VesselAdorcismModifier>();
+		}
+	}
+
+	[HarmonyPatch(typeof(CustomMurderRpc), nameof(CustomMurderRpc.RpcCustomMurder),
+		[typeof(PlayerControl), typeof(PlayerControl), typeof(MeetingCheck),
+		 typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool), typeof(bool)])]
+	[HarmonyPrefix]
+	public static void TryMurderVesselGhostPrefix(PlayerControl source, ref PlayerControl target)
+	{
+		if (!target.Data.IsDead || target.Data.Disconnected)
+		{
+			return;
+		}
+
+		if (!target.TryGetModifier<PoltergeistModifier>(out var mod))
+		{
+			return;
+		}
+
+		if (source.Data.Role is not VeteranRole)
+		{
+			return;
+		}
+
+		target = mod.Target;
+	}
+
+	//[HarmonyPatch(typeof(LookoutEvents), nameof(LookoutEvents.CheckForLookoutWatched))]
+	[HarmonyPatch(typeof(PlaguebearerRole), nameof(PlaguebearerRole.CheckInfected))]
+	[HarmonyPatch(typeof(HunterRole), nameof(HunterRole.RpcCatchPlayer))]
+	[HarmonyPrefix]
+	public static void GhostKillPlayerTrackedByRolePrefix(ref PlayerControl source)
+	{
+		if (!source.Data.IsDead || source.Data.Disconnected)
+		{
+			return;
+		}
+
+		if (!source.TryGetModifier<PoltergeistModifier>(out var mod))
+		{
+			return;
+		}
+
+		source = mod.Target;
+	}
+
+	//[HarmonyPatch(typeof(CelebrityEvents), nameof(CelebrityEvents.AfterMurderEventHandler))]
+	[HarmonyPatch(typeof(TelepathEvents), nameof(TelepathEvents.AfterMurderEventHandler))]
+	[HarmonyPatch(typeof(DeputyEvents), nameof(DeputyEvents.AfterMurderEventHandler))]
+	[HarmonyPatch(typeof(FrostyEvents), nameof(FrostyEvents.AfterMurderEventHandler))]
+	[HarmonyPatch(typeof(BaitEvents), nameof(BaitEvents.AfterMurderEventHandler))]
+	[HarmonyPrefix]
+	public static void GhostKillPlayerTrackedByEventPrefix(ref AfterMurderEvent @event)
+	{
+		var source = @event.Source;
+		if (!source.Data.IsDead || source.Data.Disconnected)
+		{
+			return;
+		}
+
+		if (!source.TryGetModifier<PoltergeistModifier>(out var mod))
+		{
+			return;
+		}
+
+		source = mod.Target;
+
+		@event = new(
+			source,
+			@event.Target,
+			@event.DeadBody
+		);
+	}
+
+	[HarmonyPatch(typeof(RottingModifier), nameof(RottingModifier.StartRotting))]
+	[HarmonyPrefix]
+	public static void GhostKilledRottingPrefix(ref PlayerControl? killer)
+	{
+		if (killer == null)
+		{
+			return;
+		}
+
+		if (!killer.Data.IsDead || killer.Data.Disconnected)
+		{
+			return;
+		}
+
+		if (!killer.TryGetModifier<PoltergeistModifier>(out var mod))
+		{
+			return;
+		}
+
+		killer = mod.Target;
+	}
+
+	[HarmonyPatch(typeof(MirrorcasterRole), nameof(MirrorcasterRole.RpcMagicMirrorAttacked))]
+	public static class GhostAttackMagicMirrorPatch
+	{
+		public static bool Prefix(ref PlayerControl source, ref bool __state)
+		{
+			if (LobbyBehaviour.Instance)
+			{
+				return true;
+			}
+
+			if (!source.Data.IsDead || source.Data.Disconnected)
+			{
+				return true;
+			}
+
+			if (!source.TryGetModifier<PoltergeistModifier>(out var mod))
+			{
+				return true;
+			}
+
+			if (mod.Player.AmOwner || mod.Target.AmOwner)
+			{
+				__state = true;
+				return false;
+			}
+
+			source = mod.Target;
+
+			return true;
+		}
+
+		public static void Postfix(PlayerControl source, PlayerControl mirrorcaster, bool __state)
+		{
+			if (!__state)
+			{
+				return;
+			}
+
+			var mod = source.GetModifier<PoltergeistModifier>()!;
+			source = mod.Target;
+
+			// Execute logic previously skipped
+			if (mirrorcaster.Data.Role is not MirrorcasterRole role)
+			{
+				Error("RpcMagicMirrorAttacked - Invalid mirrorcaster");
+				return;
+			}
+
+			role.SetProtectedPlayer(null);
+			role.UnleashesAvailable++;
+
+			var killerRole = source.GetRoleWhenAlive();
+			if (killerRole is MirrorcasterRole mirrorcaster2)
+			{
+				role.ContainedRole = mirrorcaster2.ContainedRole;
+				mirrorcaster2.ContainedRole = null;
+			}
+
+			if (source.Data.Role is IGhostRole)
+			{
+				killerRole = source.Data.Role;
+			}
+
+			role.ContainedRole = killerRole;
+
+			var opt = OptionGroupSingleton<MirrorcasterOptions>.Instance;
+			if (opt.WhoGetsNotification is MirrorOption.MirrorcasterAndKiller && mod.Player.AmOwner)
+			{
+				MirrorcasterRole.DangerAnim();
+			}
 		}
 	}
 }
