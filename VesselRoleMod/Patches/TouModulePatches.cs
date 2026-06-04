678d00b172e561ca0a33cdbccf1cd084b1adea94
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-06-04 16:27:54 -0600

    Created History system for specifically ghost possession kills

---
+++ VesselRoleMod/Patches/TouModulePatches.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using MiraAPI.Modifiers;
+using TownOfUs.Modules;
+using VesselRoleMod.Modifiers.Ghost;
+using VesselRoleMod.Modules.ControlSystem;
+
+namespace VesselRoleMod.Patches;
+
+[HarmonyPatch]
+public static class TouModulePatches
+{
+	[HarmonyPatch(typeof(GameHistory), nameof(GameHistory.AddMurder))]
+	[HarmonyPostfix]
+	public static void AddVesselMurderPostfix(PlayerControl killer, PlayerControl victim)
+	{
+		if (!killer.Data.IsDead || killer.Data.Disconnected)
+		{
+			return;
+		}
+
+		if (!killer.TryGetModifier<PoltergeistModifier>(out var mod))
+		{
+			return;
+		}
+
+		PossessionHistory.AddMurder(killer, mod.Vessel, victim);
+	}
+
+	[HarmonyPatch(typeof(GameHistory), nameof(GameHistory.ClearMurder))]
+	[HarmonyPostfix]
+	public static void ClearVesselMurderPostfix(PlayerControl player)
+	{
+		PossessionHistory.ClearMurder(player);
+	}
+
+	[HarmonyPatch(typeof(GameHistory), nameof(GameHistory.ClearAll))]
+	[HarmonyPostfix]
+	public static void ClearAllPostfix()
+	{
+		PossessionHistory.ClearAll();
+	}
+}
