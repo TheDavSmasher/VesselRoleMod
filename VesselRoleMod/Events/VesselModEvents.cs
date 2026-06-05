0f6762cc3bed28dc4a2e1025c3169a40708abbba
David Higueros <111460331+TheDavSmasher@users.noreply.github.com>
2026-06-05 14:14:21 -0600

    Added Change role handler

--- VesselRoleMod/Events/VesselModEvents.cs
+++ VesselRoleMod/Events/VesselModEvents.cs
@@ -1,6 +1,14 @@
 ﻿using MiraAPI.Events;
 using MiraAPI.Events.Vanilla.Gameplay;
 using MiraAPI.Hud;
+using MiraAPI.Modifiers;
+using TownOfUs.Events.TouEvents;
+using TownOfUs.Utilities;
+using VesselRoleMod.Modifiers.Crewmate;
+using VesselRoleMod.Modifiers.Ghost;
+using VesselRoleMod.Modules.ControlSystem;
+using VesselRoleMod.Roles.Crewmate;
+using VesselRoleMod.Utilities;
 
 namespace VesselRoleMod.Events;
 
@@ -29,6 +37,35 @@ public static class VesselModEvents
 			if (button.MaxUses <= 0)
 			{
 				button.Button.SetInfiniteUses();
+			}
+		}
+	}
+
+	[RegisterEvent]
+	public static void ChangeRoleHandler(ChangeRoleEvent @event)
+	{
+		var player = @event.Player;
+
+		if (!PlayerControl.LocalPlayer || player == null)
+		{
+			return;
+		}
+
+		if (player.Data.Role is VesselRole && player.TryGetModifier<VesselPossessedModifier>(out var mod))
+		{
+			VesselRole.RpcGhostEndPossession(mod.Ghost, player);
+		}
+
+		if (VesselControlState.IsControlled(player.PlayerId, out var controllerId))
+		{
+			var controller = MiscUtils.PlayerById(controllerId);
+			if (controller != null && controller.TryGetModifier<PoltergeistModifier>(out var ghostMod) && ghostMod.Vessel == player)
+			{
+				VesselRole.RpcGhostEndPossession(controller, player);
+			}
+			else
+			{
+				VesselControlState.ClearControl(player.PlayerId);
 			}
 		}
 	}
